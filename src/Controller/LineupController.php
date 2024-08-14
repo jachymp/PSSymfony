368ff762293a4d54854beab5060c18301b481b68
Jachym Pivonka <jachympivonka@gmail.com>
2024-08-14 15:35:57 +0200

    pridani lineupu a mensi upravy

--- src/Controller/LineupController.php
+++ src/Controller/LineupController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\LineupRepository;
+use App\Repository\PlaceRepository;
 use App\Repository\YearRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,7 +11,13 @@ use Symfony\Component\HttpFoundation\Response;
 class LineupController extends AbstractController
 {
     private array $data;
-    public function __construct(LineupRepository $lineupRepository, YearRepository $yearRepository)
+    private array $artistLineup;
+    private array $days = ['Pátek', 'Sobota', 'Neděle'];
+    private bool $isLineupPublic;
+    public function __construct(
+        LineupRepository $lineupRepository,
+        YearRepository $yearRepository,
+        PlaceRepository $placeRepository)
     {
         $lastYear = $yearRepository->getLastYear();
         $this->data = $lineupRepository->findByLastYear($lastYear['lastYearId']);
@@ -18,11 +25,38 @@ class LineupController extends AbstractController
         foreach ($this->data as $lineup) {
             $lineup->artist_start = $lineup->getTimeFrom()->format('H:i:s');
         }
+
+        foreach ($yearRepository->findLastYearData() as $year) {
+            $this->isLineupPublic = $year->isLineupPublic();
+        }
+
+        foreach ($this->days as $day) {
+            foreach ($placeRepository->findAll() as $place) {
+                $this->artistLineup[$day][$place->getName()] =
+                    array_merge($lineupRepository->findLineupByDay($lastYear['lastYearId'], $day, $place->getName()),
+                    $lineupRepository->findLineupByDayNight($lastYear['lastYearId'], $day, $place->getName()));
+
+                if(empty($this->artistLineup[$day][$place->getName()])) {
+                    unset($this->artistLineup[$day][$place->getName()]);
+                }
+            }
+            if(empty($this->artistLineup[$day])) {
+                unset($this->artistLineup[$day]);
+            }
+        }
     }
     public function artistOrder(): Response
     {
         return $this->render('lineup/index.html.twig', [
             'artists' => $this->data,
+        ]);
+    }
+
+    public function lineup(): Response
+    {
+        return $this->render('lineup/lineup.html.twig', [
+            'lineup' => $this->artistLineup,
+            'lineup_public' => $this->isLineupPublic,
         ]);
     }
 }
